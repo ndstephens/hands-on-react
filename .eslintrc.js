2e7bf597ca1dc6eec896b0083cc4a6c86e110dd3
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-09-16 18:59:10 -0400

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+    'simple-import-sort',
+    'react',
+    'jsx-a11y',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
+  rules: {
+    // "react/no-unescaped-entities": "off",
+    'no-console': 'warn',
+    // SIMPLE-IMPORT-SORT
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node.js builtins
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+          ],
+          // Type imports
+          ['^@?\\w.*\\u0000$', '^[^.].*\\u0000$', '^\\..*\\u0000$'],
+          // NPM packages. `react` related packages come first
+          ['^react', '^@react', '^@?\\w'],
+          // Side effect imports
+          ['^\\u0000'],
+          // Absolute imports
+          [
+            '^(@|components|config|elements|hooks|lib|pages|src|state|stories|styles|types|utils|store|theme)(/.*|$)',
+          ],
+          // Parent imports. Put `..` last
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    // SIMPLE-IMPORT-SORT
+  },
+};
